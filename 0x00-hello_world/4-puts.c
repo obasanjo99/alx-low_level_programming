87fe9c87a54f5c9375e506c3f87415f4f427ef56
obasanjo99 <warizrasheed.com>
2023-08-10 20:15:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+include <stio.h>
+/**
+ * main - Entry point 
+ * return - Always 0 (success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+		return (0);
+
