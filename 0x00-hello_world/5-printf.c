6679afebc62706a4f14ab7c859c0768fbdc7d647
obasanjo99 <warizrasheed.com>
2023-08-11 17:58:13 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry point 
- * return - Always 0 (success}
+ * main - Entry point
+ * description - with the proper grammar , but the outcome is a piece of art
+ * Return: Always 0 (success}
  */
-int main (void)
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
 
