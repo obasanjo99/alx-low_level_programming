f50b164823633bba25143e011dd754b986b87f76
obasanjo99 <warizrasheed.com>
2023-08-11 18:37:58 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * description - size of c
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+prinf("Size of a char: %lu byte(s)", sizeof(char));
+prinf("Size of an int: %lu byte(s)", sizeof(int));
+prinf("Size of a long: %lu byte(s)", sizeof(long int));
+prinf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+prinf("Size of a float: %lu byte(s)", sizeof(float));
+return (0);
