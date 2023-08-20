8f93dd18da09aaa3d834a5763ca50879174d5b1f
obasanjo99 <warizrasheed.com>
2023-08-20 00:13:29 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,12 +12,10 @@ int main(void)
 
     for (i = 0; i < 10; i++)
     {
-        putchar(i + '0');
+        putchar( i + '0');
         if (i < 9);
 
     }
     putchar('\n');
     return (0);
 }
-
-
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *description - prints numbers of base 16 in lowercase
+  *Return: 0 (success)
+*/
+int main(void)
+{
+int j;
+for (j = 0; j <= 15; j++)
+{
+printf("%x", j);
+}
+printf("\n");
+return (0);
+}
 
